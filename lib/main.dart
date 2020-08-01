3aec6d440306dda293f52af251864822bd4d4ef4
Amr Ezz <amrezz0111@gmail.com>
2020-08-01 08:00:23 +0200

    restoring the old app

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,6 @@ void main() {
   );
 }
 
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: SafeArea(
-//         minimum: EdgeInsets.only(top: 50),
-//         child: Scaffold(
-//           backgroundColor: Colors.teal,
-//           body: Center(
-//             child: Container(
-//               width: 200,
-//               height: 200,
-//               color: Colors.blueAccent,
-//               child: Center(
-//                 child: Text('Hello World'),
-//               ),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -38,35 +14,15 @@ class MyApp extends StatelessWidget {
         minimum: EdgeInsets.only(top: 50),
         child: Scaffold(
           backgroundColor: Colors.teal,
-          body: Row(
-            children: <Widget>[
-              Container(
-                width: 100,
-                color: Colors.red,
+          body: Center(
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.blueAccent,
+              child: Center(
+                child: Text('Hello World'),
               ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.amber,
-                    ),
-                    Container(
-                      width: 100,
-                      height: 100,
-                      color: Colors.green,
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                width: 100,
-                alignment: Alignment.topRight,
-                color: Colors.blueAccent,
-              ),
-            ],
+            ),
           ),
         ),
       ),
