b4effac35d08b7e447db2e0d4604b681e86569b9
Amr Ezz <amrezz0111@gmail.com>
2020-08-01 06:29:18 +0200

    adding 4 colored containers

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,74 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MyApp(),
   );
+}
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: SafeArea(
+//         minimum: EdgeInsets.only(top: 50),
+//         child: Scaffold(
+//           backgroundColor: Colors.teal,
+//           body: Center(
+//             child: Container(
+//               width: 200,
+//               height: 200,
+//               color: Colors.blueAccent,
+//               child: Center(
+//                 child: Text('Hello World'),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        minimum: EdgeInsets.only(top: 50),
+        child: Scaffold(
+          backgroundColor: Colors.teal,
+          body: Row(
+            children: <Widget>[
+              Container(
+                width: 100,
+                color: Colors.red,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.amber,
+                    ),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 100,
+                alignment: Alignment.topRight,
+                color: Colors.blueAccent,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
